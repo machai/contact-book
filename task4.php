9bd16883d3a34db7dac30551ed856c2bd57067f9
machai <machaibulawayo@gmail.com>
2019-11-10 02:20:59 +0200

    Add files via upload

---
+++ task4.php
@@ -0,0 +1,34 @@
+<?php
+
+/*************************************************************
+* Author: Machai Justine Bulawayo
+* Student Number: 54651719
+* Module Name: ICT2613
+
+************************************************************/
+
+?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Task 4</title>
+    </head>
+    <body>
+        <?php include 'menu.inc';  ?>
+    
+     <div id="wrapper">
+	 
+	  
+				
+	</div>
+	
+        
+         <iframe src="task4.txt" height = "400" scrolling = "yes" width = "1200">
+        <p>Your browser does not support iframes. </p> </iframe>
+    
+    </div>
+    
+    </body>
+</html>
